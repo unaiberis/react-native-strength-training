73d2507b6754e46346110750cae058143e2d7cbd
CI Autofix <ci-autofix@musikak.com>
2026-07-08 18:37:13 +0000

    feat: upgrade expo sdk 52 to sdk 54

--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,12 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [["babel-preset-expo", { jsxImportSource: "nativewind" }]],
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
+    ],
     plugins: [
-      // NOTA: "nativewind/babel" (react-native-css-interop/babel) en v0.2.6
-      // requiere react-native-worklets/plugin (RN ≥0.83). Nuestra RN es 0.76.6.
-      // Inlineamos solo los plugins que necesitamos desde la capa base v0.1.22.
-      require("react-native-css-interop/dist/babel-plugin").default,
-      [
-        "@babel/plugin-transform-react-jsx",
-        {
-          runtime: "automatic",
-          importSource: "react-native-css-interop",
-        },
-      ],
-      "react-native-reanimated/plugin",
+      "react-native-worklets/plugin",
     ],
   };
 };
